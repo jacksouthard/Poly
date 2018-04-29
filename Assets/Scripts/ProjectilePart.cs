9f647b41d373babc357a1a4fc5df92b404934bbd
Jack Southard <jacksouthard265@gmail.com>
2018-04-29 15:00:36 -0700

    Add blaster part

--- Assets/Scripts/ProjectilePart.cs
+++ Assets/Scripts/ProjectilePart.cs
@@ -6,9 +6,11 @@ public class ProjectilePart : Part {
 	bool shouldFire = false;
 
 	public int projectileIndex;
-	public float spawnDistance;
 	public float fireRate;
 	float timeUntilFire = 0f;
+
+	public Transform[] projectileSpawns;
+	int spawnIndex = 0;
 
 	void Update () {
 		if (master) {
@@ -26,7 +28,7 @@ public class ProjectilePart : Part {
 	}
 
 	void Fire () {
-		Vector3 spawnPos = transform.position + transform.up * spawnDistance;
+		Vector3 spawnPos = GetProjectileSpawnSpawn ();
 		Quaternion spawnRot = transform.rotation;
 		pc.RelayProjectileSpawn (projectileIndex, spawnPos, spawnRot);
 	}
@@ -42,6 +44,18 @@ public class ProjectilePart : Part {
 		if (master && coll.GetComponentInParent<PolyController> () != null) {
 			// poly leave fire zone
 			shouldFire = false;
+		}
+	}
+
+	Vector3 GetProjectileSpawnSpawn () {
+		if (projectileSpawns.Length == 1) {
+			return projectileSpawns [0].position;
+		} else {
+			spawnIndex++;
+			if (spawnIndex >= projectileSpawns.Length) {
+				spawnIndex = 0;
+			}
+			return projectileSpawns [spawnIndex].position;
 		}
 	}
 }
