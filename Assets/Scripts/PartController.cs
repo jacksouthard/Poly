f8eb7cddfb4530ccd961652615c212b7d30c9308
Jack Southard <jacksouthard265@gmail.com>
2018-06-09 09:44:28 -0700

    Add part despawning to reduce part clumpage

--- Assets/Scripts/PartController.cs
+++ Assets/Scripts/PartController.cs
@@ -6,6 +6,11 @@ public class PartController : NetworkBehaviour {
 	public bool inited = false;
 	public bool collected = false;
 
+	// despawning
+	float maxLifetime = 180f;
+	float minLifetime = 30f;
+	float curLifetime;
+
 	[SyncVar(hook="UpdateData")]
 	public int id;
 
@@ -13,6 +18,21 @@ public class PartController : NetworkBehaviour {
 		transform.parent = GameObject.Find("PartsContainer").transform;
 		if (!inited) {
 			UpdateData (id);
+		}
+
+		if (isServer) {
+			curLifetime = Random.Range (minLifetime, maxLifetime);
+		}
+	}
+
+	void Update () {
+		if (isServer) {
+			curLifetime -= Time.deltaTime;
+			if (curLifetime <= 0f) {
+				// despawn
+				PartsManager.instance.PartDestoryed ();
+				Destroy (gameObject);
+			}
 		}
 	}
 
